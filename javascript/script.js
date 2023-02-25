f308a1ac357f97ffa5345959cfc101bcb83370b5
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2023-02-25 10:31:02 +0100

    added a new javascript using toggle class name

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,36 @@
-const body = document.querySelector('body');
-const ul = document.querySelector('ul');
-const moon = document.querySelector('.moon');
-const sun = document.querySelector('.sun');
+const body = document.querySelector("body");
+const toggle = document.querySelector("#toggle");
+const sunIcon = document.querySelector(".toggle .bxs-sun");
+const moonIcon = document.querySelector(".toggle .bxs-moon");
 
+toggle.addEventListener("change", ()=>{
+  body.classList.toggle("dark");
+  sunIcon.className=
+    sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+  moonIcon.className=
+    moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
 
-
-moon.addEventListener('click', ()=>{
-  moon.classList.add('active-moon');
-  body.classList.add('drk-bg');
-  ul.classList.add('drk-bg')
 });
 
-sun.addEventListener('click', ()=>{
-  moon.classList.remove('active-moon');
-  body.classList.remove('drk-bg');
-  ul.classList.remove('drk-bg');
-  sun.style.background = 'black';
-  sun.style.color = 'white';
-});
+
+
+// const body = document.querySelector('body');
+// const ul = document.querySelector('ul');
+// const moon = document.querySelector('.moon');
+// const sun = document.querySelector('.sun');
+
+
+
+// moon.addEventListener('click', ()=>{
+//   moon.classList.add('active-moon');
+//   body.classList.add('drk-bg');
+//   ul.classList.add('drk-bg')
+// });
+
+// sun.addEventListener('click', ()=>{
+//   moon.classList.remove('active-moon');
+//   body.classList.remove('drk-bg');
+//   ul.classList.remove('drk-bg');
+//   sun.style.background = 'black';
+//   sun.style.color = 'white';
+// });
